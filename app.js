0895706b5b699c1e6a5f6928139dbc8a386b5192
castey <davidandrewcastro@gmail.com>
2023-12-30 20:21:26 -0500

    updated log

--- app.js
+++ app.js
@@ -246,7 +246,7 @@ io.on('connection', async (socket) => {
 
         socket.emit("favDenom", favoriteDenom);
 
-        console.log('Authenticated user connected' + userID);
+        console.log('Authenticated user connected: ' + userID);
 
         socket.on('chat message', async (event) => {
 
